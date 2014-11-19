bdab8a8cc61687c0528efb40aaa99078edcfa6fb
Lindsey Bateman <lindsey@wercker.com>
2014-11-19 16:29:33 +0100

    added learn title again and blocks animation

--- local_modules/learnblocks/index.js
+++ local_modules/learnblocks/index.js
@@ -25,41 +25,48 @@ function render() {
   var state = this.state;
 
   return dom.div({className: 'flex-inner'},
+    dom.h1(null, 'Get to know wercker'),
     dom.div({className: 'learnblocks'},
       dom.div({className: 'learnblocks-item learnblocks-item_basics'},
-        dom.div({className: 'learnblocks-item-icon'},
-          dom.img( {src:'/images/icon-basics.svg', alt:'Basics'})
-        ),
+        dom.a({
+          className: 'learnblocks-item-icon',
+          href: '/learn/index.html'
+        }, dom.img( {src:'/images/icon-basics.svg', alt:'Basics'})),
         dom.div({className: 'learnblocks-item-title'}, 'Basics')
       ),
       dom.div({className: 'learnblocks-item learnblocks-item_yml'},
-        dom.div({className: 'learnblocks-item-icon'},
-          dom.img( {src:'/images/icon-yml.svg', alt:'wercker.yml'})
-        ),
+        dom.a({
+          className: 'learnblocks-item-icon',
+          href: '/learn/index.html'
+        }, dom.img( {src:'/images/icon-yml.svg', alt:'wercker.yml'})),
         dom.div({className: 'learnblocks-item-title'}, 'Wercker.yml')
       ),
       dom.div({className: 'learnblocks-item learnblocks-item_boxes'},
-        dom.div({className: 'learnblocks-item-icon'},
-          dom.img( {src:'/images/icon-boxes.svg', alt:'Boxes'})
-        ),
+        dom.a({
+          className: 'learnblocks-item-icon',
+          href: '/learn/index.html'
+        }, dom.img( {src:'/images/icon-boxes.svg', alt:'Boxes'})),
         dom.div({className: 'learnblocks-item-title'}, 'Boxes')
       ),
       dom.div({className: 'learnblocks-item learnblocks-item_steps'},
-        dom.div({className: 'learnblocks-item-icon'},
-          dom.img( {src:'/images/icon-steps.svg', alt:'Steps'})
-        ),
+        dom.a({
+          className: 'learnblocks-item-icon',
+          href: '/learn/index.html'
+        }, dom.img( {src:'/images/icon-steps.svg', alt:'Steps'})),
         dom.div({className: 'learnblocks-item-title'}, 'Steps')
       ),
       dom.div({className: 'learnblocks-item learnblocks-item_build'},
-        dom.div({className: 'learnblocks-item-icon'},
-          dom.img( {src:'/images/icon-build.svg', alt:'Build'})
-        ),
+        dom.a({
+          className: 'learnblocks-item-icon',
+          href: '/learn/index.html'
+        }, dom.img( {src:'/images/icon-build.svg', alt:'Build'})),
         dom.div({className: 'learnblocks-item-title'}, 'Build')
       ),
       dom.div({className: 'learnblocks-item learnblocks-item_deploy'},
-        dom.div({className: 'learnblocks-item-icon'},
-          dom.img( {src:'/images/icon-deploy.svg', alt:'Deploy'})
-        ),
+        dom.a({
+          className: 'learnblocks-item-icon',
+          href: '/learn/index.html'
+        }, dom.img( {src:'/images/icon-deploy.svg', alt:'Deploy'})),
         dom.div({className: 'learnblocks-item-title'}, 'Deploy')
       )
     )
