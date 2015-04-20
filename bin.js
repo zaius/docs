99a9d179a45b355d68ded9aa6a449603e141914a
Yoshua Wuyts <i@yoshuawuyts.com>
2015-04-20 11:40:27 -0700

    build: init switch to brick-router

---
+++ bin.js
@@ -0,0 +1,54 @@
+#!/usr/bin/env node
+
+const summary = require('server-summary');
+const cliclopts = require('cliclopts');
+const router = require('@docs/build');
+const minimist = require('minimist');
+const assert = require('assert');
+const http = require('http');
+const url = require('url');
+
+const copts = cliclopts([
+  {
+    name: 'port',
+    abbr: 'p',
+    default: 1337,
+    help: 'port for the server to listen on'
+  }
+]);
+
+const argv = minimist(process.argv.slice(2), copts.options());
+const cmd = argv._[0];
+
+// help
+if (argv.help || !cmd) {
+  console.log('Usage: command [options]')
+  copts.print();
+  process.exit();
+}
+
+// listen
+if (cmd === 'listen' || cmd === 'server') {
+  server().listen(argv.port, summary);
+}
+
+// build
+if (cmd === 'build') router.build(__dirname + '/build', function(err, res) {
+  if (err) {
+    console.log('error:', err);
+    process.exit(1);
+  }
+  process.exit();
+});
+
+// create a server
+// null -> null
+function server() {
+  return http.createServer(function(req, res) {
+    const pathname = url.parse(req.url).pathname;
+    router.match(pathname, function(err, body) {
+      assert.ifError(err);
+      res.end(body);
+    });
+  });
+}
