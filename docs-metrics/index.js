2f21b800e6e4f718448a391284a13ac4b2a07db9
Lindsey Bateman <lindsey@wercker.com>
2015-08-17 12:03:56 -0700

    remove metrics

--- docs-metrics/index.js
+++ docs-metrics/index.js
@@ -6,15 +6,15 @@ window.client = new window.Keen({
 });
 
 // init Google analytics
-var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-28568056-1']);
-_gaq.push(['_setDomainName', 'devcenter.wercker.com']);
-_gaq.push(['_trackPageview']);
+// var _gaq = _gaq || [];
+// _gaq.push(['_setAccount', 'UA-28568056-1']);
+// _gaq.push(['_setDomainName', 'devcenter.wercker.com']);
+// _gaq.push(['_trackPageview']);
 
-var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-ga.src = (document.location.protocol === 'https:' ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-var s = document.getElementsByTagName('script')[0];
-s.parentNode.insertBefore(ga, s);
+// var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+// ga.src = (document.location.protocol === 'https:' ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+// var s = document.getElementsByTagName('script')[0];
+// s.parentNode.insertBefore(ga, s);
 
 /**
  * Setup buttons and links
